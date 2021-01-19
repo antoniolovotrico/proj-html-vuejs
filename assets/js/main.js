ee8cfb8911f57f2b3648c06e066f8126b04f5097
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-01-19 14:33:47 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// 0 - Analizzare le immagini ricevute, selezionare solo quelle idonee allo screenshot del progetto ricevuto e caricarle nella cartella img. 
+
+// 1 - Analizzare lo screenshot ricevuto e riprodurre il layout base del sito in html. 
+
+// 2 - Una volta invividuate le macro-sezioni iniziare a lavorare su ognuna di essa dalla navbar al footer(Navbar e Foote devono essere riempite dalle opzioni data in un istanza vueJs che dinamicamente li passerà all'html).
+
+// 3 - Inizializzare quindi una nuova istanza vue fornendo in data le opzioni necessarie ed eventuali metodi. 
+
+// 4 - Procedere per sezioni fino alla fine del progetto e eseguise commit ad ogni sezione completata. 
+
+// 5 - Una volta completato il progetto inizia la fase di perfezionamento del codice css al fine di una visualizzazione il più fedele possibile all'originale. 
+
+// 6 - eseguire refactoring e migliorare i commenti.
